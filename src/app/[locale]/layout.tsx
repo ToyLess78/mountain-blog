caf6a1bce4bde1a9eaf157ecb13f8b9a752f5855
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2025-03-25 17:56:03 +0200

    feat(layout): Add RootLayout component and functions for layout customization

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,53 @@
+import type React from "react"
+import "../globals.css"
+import {notFound} from "next/navigation"
+import {locales} from "~/config"
+import {NextIntlClientProvider} from 'next-intl';
+import {getTranslations} from 'next-intl/server';
+import {Inter} from 'next/font/google';
+import {routing} from '~/i18n/routing';
+
+const inter = Inter({ subsets: ["latin"] })
+
+type Props = {
+    children: React.ReactNode
+    params: { locale: string }
+}
+
+export function generateStaticParams() {
+    return routing.locales.map((locale) => ({locale}));
+}
+
+export async function generateMetadata(props: Omit<Props, 'children'>) {
+    const {locale} = await props.params;
+
+    const t = await getTranslations({locale, namespace: 'metadata'});
+
+    return {
+        title: t('title'),
+        description: t("description")
+    };
+}
+
+export default async function RootLayout({ children, params }: Props) {
+    const {locale} = await params;
+
+    if (!locales.includes(locale as any)) notFound();
+
+    let messages;
+    try {
+        messages = (await import(`../../messages/${locale}.json`)).default;
+    } catch (error) {
+        notFound();
+    }
+
+    return (
+        <html lang={locale}>
+        <body className={inter.className}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+            {children}
+        </NextIntlClientProvider>
+        </body>
+        </html>
+    );
+}
